7dc91206e6cc69289f5c7baed1ec9a62bde4583d
Nariah <527nhenderson@frhsd.com>
2026-02-05 14:02:58 -0500

    css fixes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const playerName = prompt("What is your name") || "Player";
-const yourName = `<p>Welcome <b>${playerName}</b> to Mario Kart Rock Paper Scissors</p>`
-document.getElementById("player-name").innerHTML = yourName
+let playerName = prompt("What is your name");
 if (playerName) {
   playerName = "Player";
 }
+const yourName = `<p>Welcome <b>${playerName}</b> to Mario Kart Rock Paper Scissors</p>`;
+document.getElementById("player-name").innerHTML = yourName;
 
-
+document.getElementById("playername").textContent = playerName;
